8d4b67eb17c0f38e4a56879be6a8128e230f0f99
Kurt Peek <kurt@fleetsmithhq.com>
2020-01-02 11:14:01 -0800

    Added 'Hello world' route for testing

--- python-simple/micromdm-webhook-simple.py
+++ python-simple/micromdm-webhook-simple.py
@@ -3,12 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     raw_payload = request.json['acknowledge_event']['raw_payload']
     payload = base64.b64decode(raw_payload)
     print(payload)
     return ''
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
